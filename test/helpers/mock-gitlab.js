cd3e6e0d11fe75c0137d0f9da3e11c18b1077313
Alden <aldenbe@gmail.com>
2024-07-26 05:25:58 -0700

    feat(gitlab helpers): added exported function getUploadResponse to simulate gitlab api v4 /projects/:id/uploads endpoint responses
    #753

--- test/helpers/mock-gitlab.js
+++ test/helpers/mock-gitlab.js
@@ -24,3 +24,35 @@ export function authenticate (
 ) {
   return nock(urlJoin(gitlabUrl, gitlabApiPathPrefix), {reqheaders: {'Private-Token': gitlabToken}});
 };
+
+/**
+ * @typedef {Object} GitlabUploadResponse
+ * @property {String} alt - unclear. In practice this seems to be the filename uploaded with the path to the file stripped, but this does not match the documentation
+ * @property {String} url - Unclear, documented as 	The ID or URL-encoded path of the project.
+ * in practice, seems to be the path from the project's endpoint but only in the case of using project id
+ * the latter may be an issue for private projects?
+ * @property {String} full_path - The absolute path to the uploaded file
+ * @property {String} markdown - alt property and url property formatted as a markdown link
+ */
+
+/**
+ * Simulate gitlab abi v4 /projects/:id/uploads endpoint
+ * https://docs.gitlab.com/ee/api/projects.html#upload-a-file
+ *
+ * @param {String} file The file to be uploaded.
+ * @param {(String|Number)} id The ID or URL-encoded path of the project.
+ * @return {GitlabUploadResponse}
+ */
+export function getUploadResponse (file, id = "unused as unreliable") {
+  let split_file = file.split("/")
+  file = split_file[split_file.length - 1]
+  let upload_secret = "66dbcd21ec5d24ed6ea225176098d52b"; // Example pulled from documentation random per file
+  let project_id = 4; // Example pulled from documentation, ID number of gitlab project
+  let url = `/uploads/${upload_secret}/${file}`;
+  return {
+    alt: file,
+    url: url,
+    full_path: `/-/project/${project_id}${url}`,
+    markdown: `[${file}](${url})`
+  }
+}
